ab341ef7dc1c19f5609ff12642b673fa85f1cd93
Dana Hartweg <dana.hartweg@gmail.com>
2014-07-11 15:28:38 -0400

    Prepared development environment variables.

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+  // if the env.php file exists, overwrite the current environment
+  $envFile = __DIR__.'/env.php';
+
+  if(file_exists($envFile)) {
+    require $envFile;
+  };
+
   // check for non-expired token
   
   // if the token exists, load application
