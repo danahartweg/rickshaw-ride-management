87b37bb8e0f5fae8f1c2ee5af88c9579958ce996
Dana Hartweg <dana.hartweg@gmail.com>
2014-07-12 22:05:00 -0400

    Added current location marker to map.

--- app/views/form.js
+++ app/views/form.js
@@ -4,12 +4,37 @@ export default Ember.View.extend({
 
   domSetup: function() {
 
+    var self = this;
+
+    // for now, default to raleigh coords
     var mapOptions = {
       center: new google.maps.LatLng(35.779059, -78.638714),
-      zoom: 10
+      zoom: 11
     };
 
     this.set('map', new google.maps.Map(this.$('#map-canvas')[0], mapOptions));
+
+    // attempt to geolocate client
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        var position = new google.maps.LatLng(pos.coords.latitude,
+                                          pos.coords.longitude);
+
+        // center the map to the current location
+        self.get('map').setCenter(position);
+
+        // add current location marker to the map
+        var marker = new google.maps.Marker({
+          position: position,
+          icon: {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 6
+          },
+          draggable: false,
+          map: self.get('map')
+        });
+      });
+    };
 
   }.on('didInsertElement'),
 
